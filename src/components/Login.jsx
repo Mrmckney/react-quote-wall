123406b582cc4b25fe4f4eb0c154fc4270d0dd63
Matthew McKney <mrmckney@gmail.com>
2021-09-15 14:27:14 -0400

    added handleSubmit to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,35 @@
+import { useState } from 'react'
+import { FloatingLabel, Form, Button } from 'react-bootstrap'
+
 function Login(){
+
+    const [password, setPassword] = useState(null)
+    const [email, setEmail] = useState(null)
+
+    const handleSubmit = () => {
+        fetch('http://localhost:5000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({password, email})
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => alert(err))
+    }
+
     return(
-        <h1>Hello</h1>
+        <div style={{ marginLeft: '500px', marginRight: '500px', marginTop: 100}}>
+            <h1 style={{ marginLeft: 150, marginBottom: 20}}>Sign Up</h1>
+            <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+                <Form.Control type="email" placeholder="name@example.com" onChange={e => setEmail(e.target.value)}/>
+            </FloatingLabel>
+            <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+            </FloatingLabel>
+            <Button style={{ marginTop: 30 }} onSubmit={handleSubmit} >Sign Up</Button>
+        </div>
     )
 }
 
