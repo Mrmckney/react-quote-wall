aab8e83cc8905d76154913640a3e2c6eb0f54b8e
Matthew McKney <mrmckney@gmail.com>
2021-09-15 13:53:39 -0400

    added createQuote and Quotes

--- src/components/CreateQuote.jsx
+++ src/components/CreateQuote.jsx
@@ -1,6 +1,36 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
 function CreateQuote() {
+
+    const [credit, setCredit] = useState(null)
+    const [quote, setQuote] = useState(null)
+
+    const handleSubmit = () => {
+        fetch('http://localhost:5000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({credit, quote})
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => alert(err))
+    }
+
     return(
-        <h1>Hello</h1>
+        <Form style={{ margin: "100px" }}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Quote</Form.Label>
+                <Form.Control as="textarea" rows={3} maxLength="100" placeholder="Write quote here" onChange={e => setQuote(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Credit</Form.Label>
+                <Form.Control type="email" maxLength="26" placeholder="Name here" onChange={e => setCredit(e.target.value)}/>
+            </Form.Group>
+            <Button onClick={handleSubmit}>Submit</Button>
+        </Form>
     )
 }
 
--- src/components/QuoteWall.jsx
+++ src/components/QuoteWall.jsx
@@ -1,6 +1,27 @@
+import { Card } from 'react-bootstrap'
+
 function QuoteWall() {
     return(
-        <h2>Quotes</h2>
+        <div style={{ display: 'flex' }}>
+        <Card style={{ width: '18rem', height: '15rem', margin: 20 }}>
+            <Card.Body>
+                <Card.Title>Quote</Card.Title>
+                <Card.Text>
+                    "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+                </Card.Text>
+                <Card.Subtitle className="mb-2 text-muted">By Matthew McKney</Card.Subtitle>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem', height: '15rem', margin: 20 }}>
+            <Card.Body>
+                <Card.Title>Quote</Card.Title>
+                <Card.Text>
+                    
+                </Card.Text>
+                <Card.Subtitle className="mb-2 text-muted">By</Card.Subtitle>
+            </Card.Body>
+        </Card>
+        </div>
     )
 }
 
